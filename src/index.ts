cdc0973ecbfcc1aba47005a6609460c797a544ba
Delyan Peev <Delyan.Peev@paysafe.com>
2022-12-05 17:58:28 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import config from "./config/config";
+import router from "./routes";
+import * as bodyParser from "body-parser";
+import * as compression from "compression";
+import * as cors from "cors";
+import * as express from "express";
+import * as logger from "morgan";
+
+const port = process.env.PORT || config.port || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Pretty print for JSON repsonses
+app.set("json spaces", 4);
+// Logger for requests
+app.use(logger("common"));
+// CORS middleware
+app.use(cors());
+// Gzip compression of responses
+app.use(compression());
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`API is listening on http://localhost:${port}`);
+});
